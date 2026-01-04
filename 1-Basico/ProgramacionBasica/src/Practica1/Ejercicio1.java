3b35cc205e7dd09a68af4f908e5e8e859838312d
CUALQUIERA-444 <aacodurand@gmail.com>
2026-01-04 18:17:28 +0100

    Mi primer commit en eclipse

---
+++ 1-Basico/ProgramacionBasica/src/Practica1/Ejercicio1.java
@@ -0,0 +1,11 @@
+package Practica1;
+
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Me gusta la programación");
+	System.out.println("Cada día");
+	System.out.println("MÁSSS");
+	}
+}
