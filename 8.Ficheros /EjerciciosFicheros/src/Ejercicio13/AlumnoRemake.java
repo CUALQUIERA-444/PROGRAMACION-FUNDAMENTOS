c8445c0127095f55bf89da1d384cbe3bceca2fd3
CUALQUIERA-444 <aacodurand@gmail.com>
2026-06-06 10:50:36 +0200

    Hola

--- 8.Ficheros /EjerciciosFicheros/src/Ejercicio13/AlumnoRemake.java
+++ 8.Ficheros /EjerciciosFicheros/src/Ejercicio13/AlumnoRemake.java
@@ -1,9 +1,27 @@
 package Ejercicio13;
 
 public class AlumnoRemake {
-
-	public AlumnoRemake() {
-		// TODO Auto-generated constructor stub
-	}
-
+ private String nombre; 
+ private String apellido;
+ private double nota;
+ 
+ public AlumnoRemake(String nombre, String apellido, double nota) {
+	super();
+	this.nombre = nombre;
+	this.apellido = apellido;
+	this.nota = nota;
+}
+ public String getNombre() {
+	return nombre;
+ }
+ public String getApellido() {
+	return apellido;
+ }
+ public double getNota() {
+	return nota;
+ }
+ @Override
+ public String toString() {
+	return "AlumnoRemake [nombre=" + nombre + ", apellido=" + apellido + ", nota=" + nota + "]";
+ }
 }
